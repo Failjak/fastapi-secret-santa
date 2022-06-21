f4f271c9d043ec6f6e6610832c0ee109c08cceec
failjak <nikita.gushtyn200370@gmail.com>
2022-06-21 17:31:36 +0300

    fixed error

--- database/base_class.py
+++ database/base_class.py
@@ -6,7 +6,6 @@ class Base:
     """ Base class that overloads the table name """
     __name__: str
 
-    @classmethod
     @declared_attr
     def __tablename__(cls):
         return cls.__name__.lower()
